c537561880005b4d7070e38dc75ee3ba6b53dea7
LoneTree <brandon.j.silva@gmail.com>
2014-05-09 17:01:50 -1000

    Committing Plot 2 and 3 code and png files

---
+++ plot 3.R
@@ -0,0 +1,18 @@
+##Read the full file in
+full.file <- read.table("Household_power_consumption.txt", header = TRUE, sep = ";", colClasses = "character")
+
+##Subset the data to only February 1, 2007 and February 2, 2007
+trim.file <- subset(full.file, (full.file$Date == "1/2/2007" | full.file$Date == "2/2/2007"))
+
+## Create a new column that combines date and time and bind it to the trimmed file. 
+## I have no idea why this outputs days names in the plot.
+dnt <- strptime(paste(trim.file$Date, trim.file$Time), format = "%d/%m/%Y %H:%M:%S")
+trim.file.2 <- cbind(trim.file, dnt)
+
+png(file = "plot 3.png")
+with(trim.file.2, 
+     plot(dnt, Sub_metering_1, 
+          type = "s", 
+          xlab = "", 
+          ylab = "Global Active Power (kilowatts)"))
+dev.off()
